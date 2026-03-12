6a9038089ed44427375b7a1af3c9dd321d48b1d0
Nash-BEAT <nash.0cal@gmail.com>
2026-03-12 17:37:26 +0900

    web-socketの内容追加

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,9 +1,15 @@
 class ChatChannel < ApplicationCable::Channel
-  def subscribed
-    # stream_from "some_channel"
+  def subscribed          # クライアントが接続してきたとき
+    stream_from "chat_room"   # "chat_room" というストリームを監視し始める
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  #             クライアント側                           サーバー側
+  # receive	    chatChannel.send(data)                 def receive(data) が自動で呼ばれる
+  # 任意メソッド  chatChannel.perform("メソッド名", data)  対応するメソッドが呼ばれる
+  def receive(data)       # クライアントからメッセージが届いたとき
+    ActionCable.server.broadcast("chat_room", data)  # 全員に配信する
   end
+
+
+  def unsubscribed; end          # クライアントが接続を解除したとき
 end
